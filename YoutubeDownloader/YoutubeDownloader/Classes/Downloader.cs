5ccd619e8cd6f37eff65f8d2f7f1346e8f2035f4
Eschechola <lucas.eschechola@outlook.com>
2019-11-01 01:32:19 -0300

    Adicionando o projeto

---
+++ YoutubeDownloader/YoutubeDownloader/Classes/Downloader.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Models.MediaStreams;
+
+namespace YoutubeDownloader.Classes
+{
+    public class Downloader
+    {
+        private List<string> _listaDeVideosPlaylist = new List<string>();
+        private string _linkDaPlaylist { get; set; }
+        private string _diretorioSalvar { get; set; }
+        private List<string> _listaDeNomesVideos = new List<string>();
+
+
+        public Downloader(string linkDaPlaylist, string diretorioSalvar)
+        {
+            _linkDaPlaylist = GetIdFromLink(linkDaPlaylist);
+            _diretorioSalvar = diretorioSalvar;
+        }
+
+
+        public async void Execute()
+        {
+            await GetAllLinks();
+            DownloadAllVideos();
+        }
+
+        private string GetIdFromLink(string link)
+        {
+            return YoutubeClient.ParsePlaylistId(link);
+        }
+
+        public async Task<List<string>> ReturnAllLinks()
+        {
+            await GetAllLinks();
+            return _listaDeVideosPlaylist;
+        }
+
+        public string FormatTitle(string titulo)
+        {
+            string padraoString = @"(?i)[^0-9a-záéíóúàèìòùâêîôûãõç\s]";
+            Regex regex = new Regex(padraoString);
+            string resultado = regex.Replace(titulo, "");
+
+            return resultado;
+        }
+
+        private async Task<bool> GetAllLinks()
+        {
+            try
+            {
+                var cliente = new YoutubeClient();
+                var playlistCompleta = await cliente.GetPlaylistAsync(_linkDaPlaylist);
+
+                foreach (var video in playlistCompleta.Videos)
+                {
+                    _listaDeVideosPlaylist.Add(video.Id);
+                    _listaDeNomesVideos.Add(FormatTitle(video.Title));
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+
+        private async void DownloadAllVideos()
+        {
+
+            var cliente = new YoutubeClient();
+
+            for (int i = 0; i < _listaDeVideosPlaylist.Count; i++)
+            {
+                try
+                {
+                    var streamInfoSet = await cliente.GetVideoMediaStreamInfosAsync(_listaDeVideosPlaylist[i]);
+                    var streamInfo = streamInfoSet.Audio.WithHighestBitrate();
+                    await cliente.DownloadMediaStreamAsync(streamInfo, @_diretorioSalvar + _listaDeNomesVideos[i] + ".mp3");
+                    Console.WriteLine(_listaDeNomesVideos[i] + " Baixado com sucesso");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ocorreu algum erro....");
+                    continue;
+                }
+            }
+        }
+    }
+}
---
+++ YoutubeDownloader/YoutubeDownloader/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeDownloader.Classes;
+using YoutubeExplode;
+using YoutubeExplode.Models.MediaStreams;
+
+namespace YoutubeDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o link da playlist\n> ");
+            string linkPlaylist = Console.ReadLine();
+
+            Console.Write("\n");
+            Console.WriteLine("Digite o diretório onde serão salvos os vídeos");
+            Console.WriteLine("Exemplo: C:\\Users\\Usuario\\Downloads");
+            Console.Write("> ");
+            string diretorioDeDownload = Console.ReadLine();
+
+            Console.Write("Executando...");
+            new Downloader(linkPlaylist, diretorioDeDownload).Execute();
+            Console.ReadKey();
+        }
+    }
+}
